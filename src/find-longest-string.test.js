00411d80ae1eea9305efcce2b647f2d10cb9cdcd
Linus Gellhorn <linus.g@hotmail.com>
2021-10-12 12:46:24 +0100

    Added shortest-string code and test

--- src/find-longest-string.test.js
+++ src/find-longest-string.test.js
@@ -8,4 +8,6 @@ test("findLongestString finds the longest string in an array", () => {
 
 test("findLongestString returns the earlier string in cases of joint longest strings", () => {
   expect(findLongestString(["brave", "dance"])).toBe("brave");
+  expect(findLongestString(["a", "bc", "d", "ef"])).toBe("bc");
+  expect(findLongestString(["123", "123 4", "12345"])).toBe("123 4");
 });
